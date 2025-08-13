9536eccc46c125a015667c39bebfaac59be32116
valenbasiuk <valenbasiukcls@gmail.com>
2025-08-12 22:41:22 -0300

    asd

--- js/pages/List.js
+++ js/pages/List.js
@@ -39,7 +39,7 @@ export default {
             <div class="level-container">
                 <div class="level" v-if="level">
                     <h1>{{ level.name }}</h1>
-                    <LevelAuthors :uploader="level.author" :creadores="level.creators" :verifier="level.verifier"></LevelAuthors>
+                    <LevelAuthors :uploader="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
                     <ul class="stats">
                         <li>
